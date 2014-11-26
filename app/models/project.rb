78d9da2f8796b4b8b9b34f2b4065c4d8c87ab4d2
Francisco De La Cruz <helloquico@gmail.com>
2014-11-26 05:16:32 +0000

    Add validations

--- app/models/project.rb
+++ app/models/project.rb
@@ -3,5 +3,11 @@ class Project < ActiveRecord::Base
   has_many :works
   has_many :users, :through => :works
   
+  validates :name, length: { minimum: 5 }
+  validates :company, presence: true
+  validates :default_rate, numericality: { only_integer: true, 
+                                           greater_than: 50,
+                                           less_than: 10000 }
+                           
   scope :lowdefaultrate, -> { where("default_rate < 100") }
 end
