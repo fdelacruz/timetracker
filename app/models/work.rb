f2629167689e7ad7a1e4a818ba9286767e1bd152
Francisco De La Cruz <helloquico@gmail.com>
2014-11-27 04:02:03 +0000

    Add recentworks named route to Works controller

--- app/models/work.rb
+++ app/models/work.rb
@@ -11,7 +11,12 @@ class Work < ActiveRecord::Base
                                    less_than_or_equal_to: 8 }
                          
   scope :fullday, -> { where ("hours >= 8") }
-  scope :recent, -> { where ("datetimeperformed > '#{Time.now - 7.days}'") } 
+  # scope :recent, -> { where ("datetimeperformed > '#{Time.now - 7.days}'") }
+  
+  def self.recentdays(numdaysago)
+    since_date = Time.now - numdaysago.to_i.days
+    where("datetimeperformed > '#{since_date}'")
+  end
   
   def date_is_in_the_past
     if datetimeperformed.present? && datetimeperformed > Time.now
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root 'companies#index'
   
+  get 'recentworks/:days' => 'works#index'
+  
   resources :companies
   resources :projects
   resources :works
