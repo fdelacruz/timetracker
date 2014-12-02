ef31a0421618e1976775529cd450037b6d9f2c9a
Francisco De La Cruz <helloquico@gmail.com>
2014-12-01 23:16:27 -0500

    Handle new Project validations

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -17,8 +17,11 @@ class ProjectsController < ApplicationController
 
   def create
     @project = Project.new(params[:project].permit(:name, :slug, :company_id, :default_rate))
-    @project.save
-    flash[:notice] = 'Project Created'
-    redirect_to @project
+    if @project.save
+      flash[:notice] = 'Project Created'
+      redirect_to @project
+    else
+      render 'new'
+    end
   end
 end
