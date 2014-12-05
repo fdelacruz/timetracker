6af2d3148621f90ca49a2906e0e82a31e4ba5bcd
Francisco De La Cruz <helloquico@gmail.com>
2014-12-04 20:55:21 -0500

    Generate migration to add email column to Users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141127160210) do
+ActiveRecord::Schema.define(version: 20141205014336) do
 
   create_table "companies", force: true do |t|
     t.string   "name"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 20141127160210) do
     t.integer  "company_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "email",      default: "", null: false
   end
 
   create_table "works", force: true do |t|
