9e246a28fa85a2dc6e2249f5aadfcc8b56f9f521
Francisco De La Cruz <helloquico@gmail.com>
2014-12-08 22:18:29 -0500

    Upload files to the server

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -17,6 +17,10 @@ class WorksController < ApplicationController
 
   def create
     @work = Work.new(params[:work].permit(:project_id, :user_id, :datetimeperformed, :hours))
+    uploaded_io = params[:doc]
+    File.open(Rails.root.join('public', 'uploads', uploaded_io.original_filename), 'wb') do |file|
+      file.write(uploaded_io.read)
+    end
     respond_to do |format|
       if @work.save
         Usermailer.workcreated_email(@work).deliver
