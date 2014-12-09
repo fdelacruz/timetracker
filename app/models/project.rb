cebc775359939c68f0a68de03b49be48a6334fb6
Francisco De La Cruz <helloquico@gmail.com>
2014-12-09 17:46:00 -0500

    Export Projects content to CSV

--- app/models/project.rb
+++ app/models/project.rb
@@ -18,4 +18,13 @@ class Project < ActiveRecord::Base
   def to_s
     name   
   end
+
+  def self.export_csv(projects)
+    CSV.generate() do |csv|
+      csv << column_names
+      projects.each do |project|
+        csv << project.attributes.values_at(*column_names)
+      end
+    end
+  end
 end
